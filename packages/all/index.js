cb1419de3b87edae22dea2877f2a350b3ad896f4
Corey Psoinos <coreypsoinos@gmail.com>
2022-05-07 14:16:47 -0400

    fix(all): use try/catch to detect react version

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,11 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { testReactVersion } = require('eslint-plugin-react/lib/util/version')
 
 const extendConfig = ['@coreyander/eslint-config-vue-ts', 'prettier']
 
-const reactVersion = testReactVersion()
-
-if (reactVersion !== '999.999.999') {
-  extendConfig.unshift('@coreyander/eslint-config-react-ts')
+try {
+  const reactVersion = testReactVersion()
+  if (reactVersion !== '999.999.999') {
+    extendConfig.unshift('@coreyander/eslint-config-react-ts')
+  }
+} catch (err) {
+  // ignore
 }
 
 module.exports = {
