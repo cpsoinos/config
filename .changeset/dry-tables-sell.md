fc3e158f758efdcb9a149a151fde68fac12368cf
Corey Psoinos <corey@anderapps.com>
2022-05-07 13:18:02 -0400

    All pkg (#18)
    * fix: attempt at reorganizing packages and adding 'all' pkg for easier consumption
    
    * chore: changeset

---
+++ .changeset/dry-tables-sell.md
@@ -0,0 +1,7 @@
+---
+'@coreyander/eslint-config': patch
+'@coreyander/eslint-config-react-ts': patch
+'@coreyander/eslint-config-typescript': patch
+---
+
+dependency management
--- packages/react-ts/index.js
+++ packages/react-ts/index.js
@@ -2,8 +2,7 @@ module.exports = {
   extends: [
     '@coreyander/eslint-config-typescript',
     'plugin:react/recommended', // react-specific rules
-    'plugin:react-hooks/recommended',
-    'prettier' // must be last
+    'plugin:react-hooks/recommended'
   ],
   env: {
     browser: 1
