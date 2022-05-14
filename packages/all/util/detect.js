31951c1be956f0b44b8d4ea07dc6dc23da6daf7e
Corey Psoinos <coreypsoinos@gmail.com>
2022-05-14 18:02:54 -0400

    fix(all): lint

--- packages/all/util/detect.js
+++ packages/all/util/detect.js
@@ -1,38 +1,47 @@
 // Adapted from eslint-plugin-react/lib/util/version
+const fs = require('fs')
+const path = require('path')
+const resolve = require('resolve')
 
 function resolveBasedir(contextOrFilename) {
   if (contextOrFilename) {
-    const filename = typeof contextOrFilename === 'string' ? contextOrFilename : contextOrFilename.getFilename();
-    const dirname = path.dirname(filename);
+    const filename =
+      typeof contextOrFilename === 'string'
+        ? contextOrFilename
+        : contextOrFilename.getFilename()
+    const dirname = path.dirname(filename)
     try {
       if (fs.statSync(filename).isFile()) {
         // dirname must be dir here
-        return dirname;
+        return dirname
       }
     } catch (err) {
       // https://github.com/eslint/eslint/issues/11989
       if (err.code === 'ENOTDIR') {
         // virtual filename could be recursive
-        return resolveBasedir(dirname);
+        return resolveBasedir(dirname)
       }
     }
   }
-  return process.cwd();
+  return process.cwd()
 }
 
 // TODO, semver-major: remove context fallback
-function detect(lib) {
-  const basedir = resolveBasedir(context);
+function detect(libName, context = null) {
+  const basedir = resolveBasedir(context)
 
   try {
-    const libPath = resolve.sync('lib', { basedir });
-    const lib = require(libPath); // eslint-disable-line global-require, import/no-dynamic-require
-    cachedDetectedLibVersion = lib.version;
-    return cachedDetectedLibVersion;
+    const libPath = resolve.sync(libName, { basedir })
+    const lib = require(libPath) // eslint-disable-line global-require, import/no-dynamic-require
+    if (lib.version) {
+      return lib.version
+    }
   } catch (e) {
     if (e.code === 'MODULE_NOT_FOUND') {
       return false
     }
-    throw e;
+    throw e
   }
 }
+
+module.exports = { detect }
