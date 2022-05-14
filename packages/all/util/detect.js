fb206a1010c05045685a932febb22a07efe7743e
Corey Psoinos <corey@anderapps.com>
2022-05-14 18:10:28 -0400

    Separate frameworks from ts (#29)
    * refactor: rename vue-ts to vue, react-ts to react, and don't extend ts in each but do so overall
    
    * fix(all): lint
    
    * chore: changeset
    
    * fix(typescript): install missing peer dep: eslint-plugin-import
    
    * style(all): disable @typescript-eslint/no-var-requires

---
+++ packages/all/util/detect.js
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// Adapted from eslint-plugin-react/lib/util/version
+const fs = require('fs')
+const path = require('path')
+const resolve = require('resolve')
+
+function resolveBasedir(contextOrFilename) {
+  if (contextOrFilename) {
+    const filename =
+      typeof contextOrFilename === 'string'
+        ? contextOrFilename
+        : contextOrFilename.getFilename()
+    const dirname = path.dirname(filename)
+    try {
+      if (fs.statSync(filename).isFile()) {
+        // dirname must be dir here
+        return dirname
+      }
+    } catch (err) {
+      // https://github.com/eslint/eslint/issues/11989
+      if (err.code === 'ENOTDIR') {
+        // virtual filename could be recursive
+        return resolveBasedir(dirname)
+      }
+    }
+  }
+  return process.cwd()
+}
+
+// TODO, semver-major: remove context fallback
+function detect(libName, context = null) {
+  const basedir = resolveBasedir(context)
+
+  try {
+    const libPath = resolve.sync(libName, { basedir })
+    const lib = require(libPath) // eslint-disable-line global-require, import/no-dynamic-require
+    if (lib.version) {
+      return lib.version
+    }
+  } catch (e) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+      return false
+    }
+    throw e
+  }
+}
+
+module.exports = { detect }
