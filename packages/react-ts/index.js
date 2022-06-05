71d17ff5208dd2213102102bc4eaabed26919769
Corey Psoinos <coreypsoinos@gmail.com>
2022-06-04 20:10:56 -0400

    fix(vue-ts, react-ts): add 'prettier' to 'extends' array

--- packages/react-ts/index.js
+++ packages/react-ts/index.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: [
     '@cpsoinos/eslint-config-typescript',
     'plugin:react/recommended', // react-specific rules
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'prettier' // must be last
   ],
   env: {
     browser: 1
