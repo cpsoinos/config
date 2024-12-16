e8721e133f8d3815f98bee6c65a6e0f9d53bfc51
Corey Psoinos <coreypsoinos@gmail.com>
2024-12-15 19:09:48 -0500

    chore: correctly ignore build files for linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,6 @@ import base from '@cpsoinos/eslint-flat-configs/base'
 export default [
   ...base,
   {
-    ignores: ['.husky', '.nx'],
+    ignores: ['.husky', '.nx', 'packages/*/dist/*'],
   },
 ]
