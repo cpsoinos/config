1b92a260443580ddea53ed637a7699888c4fdbbe
Corey Psoinos <coreypsoinos@gmail.com>
2024-12-16 21:38:12 -0500

    feat(prettier-config): bundle with unbuild

--- packages/prettier/src/index.ts
+++ packages/prettier/src/index.ts
@@ -1,8 +1,11 @@
-// eslint-disable-next-line no-undef
-module.exports = {
+import type { Config } from 'prettier'
+
+const config = {
   arrowParens: 'always',
   semi: false,
   singleQuote: true,
   printWidth: 100,
   vueIndentScriptAndStyle: true,
-}
+} satisfies Config
+
+export default config
