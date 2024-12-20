2236df67e878c9401dab13b27283cb216e9ef3ca
Corey Psoinos <coreypsoinos@gmail.com>
2024-12-19 20:24:36 -0500

    fix(eslint-flat-configs): specify tsconfigRootDir for type-checked ts eslint config

--- packages/eslint-flat-configs/src/base.ts
+++ packages/eslint-flat-configs/src/base.ts
@@ -14,6 +14,7 @@ export const base: ReturnType<typeof tsEslintConfig> = tsEslintConfig(
     languageOptions: {
       parserOptions: {
         projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
